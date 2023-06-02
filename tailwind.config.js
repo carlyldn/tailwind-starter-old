8b9cf84898cbf819b510b621034f3088d02fda2e
carlyldn <88619398+carlyldn@users.noreply.github.com>
2023-06-02 14:20:44 +0100

    base template

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,22 @@ module.exports = {
     './*.{html,md,js}'
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      body: ['font-name', 'sans-serif'],
+      heading: ['font-name', 'serif'],
+      subtitle: ['font-name', 'sans-serif'],
+      accent: ['font-name', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'main': '#000',
+      },
+      aspectRatio: {
+        'portrait': '3/4',
+        'landscape': '16/9',  
+        'square': '1/1',
+      },  
+    },
   },
   plugins: [],
 }
